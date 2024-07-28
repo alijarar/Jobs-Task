143e5ec434bacdee896c703a49eb23c2cb9ab64c
ali jarar <alijarar5@gmail.com>
2024-07-29 00:09:31 +0500

    cache data in case use is offline

--- calo-task-frontend/src/hooks/useJobs.tsx
+++ calo-task-frontend/src/hooks/useJobs.tsx
@@ -1,38 +1,46 @@
 import { Job } from "@/@types/types";
 import { JobsService } from "@/services/jobs";
-import {useQuery, useMutation} from 'react-query'
-
-
+import { useQuery, useMutation } from "react-query";
 
 const jobService = new JobsService();
 
 const useGetJobs = () =>
-    useQuery(
-      ['Jobs'],
-      () => jobService.fetchJobs(),
-      { retry: 2, refetchOnWindowFocus: true, keepPreviousData: true }
+  useQuery(
+    ['Jobs'],
+    () => jobService.fetchJobs(),
+    {
+      retry: 2,
+      refetchOnWindowFocus: true,
+      keepPreviousData: true,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 30 // 30 minutes
+    }
   );
-    
-  const useCreateJob = () => {
-    return useMutation(
-      (newJob: Partial<Job>) => jobService.createJob(newJob),
-      {
-        onSuccess: () => {
-          // You might want to invalidate or refetch queries here
-        },
-        onError: (error) => {
-          console.error(error);
-        }
+
+const useCreateJob = () => {
+  return useMutation(
+    (newJob: Partial<Job>) => jobService.createJob(newJob),
+    {
+      onSuccess: () => {
+      },
+      onError: (error) => {
+        console.error(error);
       }
-    );
-  };
-  
-  const useGetJobById = (jobId: string) =>
-      useQuery<Job, any>(
-        ['JobById', jobId],
-        () => jobService.fetchJobById(jobId),
-        { retry: 2, refetchOnWindowFocus: true }
-      );
+    }
+  );
+};
+
+const useGetJobById = (jobId: string) =>
+  useQuery<Job, any>(
+    ['JobById', jobId],
+    () => jobService.fetchJobById(jobId),
+    {
+      retry: 2,
+      refetchOnWindowFocus: true,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 30 // 30 minutes
+    }
+  );
 
 export {
   useGetJobs,
